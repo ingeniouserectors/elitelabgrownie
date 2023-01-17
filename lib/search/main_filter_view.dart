e2c33274fb14d0317527e052be6a85180ed1a428
Kirtan Parakhiya <67089821+kirtan-2103@users.noreply.github.com>
2023-01-17 23:05:56 +0530

    - Add content in filter. - Added diamond shape images.

--- lib/search/main_filter_view.dart
+++ lib/search/main_filter_view.dart
@@ -1,10 +1,14 @@
+import 'package:ecom/search/search_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class MainFilterView extends StatefulWidget {
-  List<String> filters;
 
-  MainFilterView({Key? key, required this.filters}) : super(key: key);
+  List<FilterItems> jewellery = [];
+  List<FilterItems> centerShape = [];
+  List<FilterItems> metal = [];
+
+  MainFilterView({Key? key, required this.jewellery, required this.centerShape, required this.metal}) : super(key: key);
 
   @override
   State<MainFilterView> createState() => _MainFilterViewState();
@@ -15,10 +19,6 @@ class _MainFilterViewState extends State<MainFilterView> {
   @override
   Widget build(BuildContext context) {
 
-    List<String> tags = ['#sfsfsf', '#qwqw', '#fffff', '#qwqer', '#zxcv','#sfsfsf', '#qwqw', '#fffff', '#qwqer', '#zxcv'];
-    List<String> cat = ['Diamonds', 'Jewellery ', 'Dashboard', 'Education', 'Orders','Points'];
-    // List<String> attributes = ['#sfsfsf', '#qwqw', '#fffff', '#qwqer', '#zxcv','#sfsfsf', '#qwqw', '#fffff', '#qwqer', '#zxcv'];
-
     Widget _cancelApplyView(){
       return Container(
         child: Row(
@@ -28,12 +28,22 @@ class _MainFilterViewState extends State<MainFilterView> {
               // style: ButtonStyle(
               //   foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
               // ),
-              onPressed: () { },
-              child: Text('Cancel', style: TextStyle(fontSize: 20, color: Colors.red),),
+              onPressed: () {
+                setState(() {
+                  widget.jewellery.forEach((element) {element.isSelected = false;});
+                  widget.centerShape.forEach((element) {element.isSelected = false;});
+                  widget.metal.forEach((element) {element.isSelected = false;});
+                });
+                Navigator.pop(context, false);
+              },
+              child: Text('Reset', style: TextStyle(fontSize: 20, color: Colors.red),),
             ),
             SizedBox(width: 10,),
             TextButton(
-              onPressed: () { },
+              onPressed: () {
+                // Get.back();
+                Navigator.pop(context, true);
+              },
               child: Text('Apply', style: TextStyle(fontSize: 20),),
             ),
           ],
@@ -41,19 +51,45 @@ class _MainFilterViewState extends State<MainFilterView> {
       );
     }
 
-    Widget _listChild(int index, String text){
-      return Container(
-        decoration: BoxDecoration(
-          color: Colors.blueGrey.withOpacity(0.2),
-          borderRadius: BorderRadius.circular(2),
+    Widget _listChild(int index, FilterItems item){
+      return GestureDetector(
+        onTap: (){
+          setState(() {
+            item.isSelected = !item.isSelected;
+          });
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            color: item.isSelected ? Color(0xffD9F3F2) : Color(0xffEBEBEB),
+            borderRadius: BorderRadius.circular(2),
+          ),
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          alignment: Alignment.center,
+          child: Text(item.name, style: TextStyle(fontSize: 15), textAlign: TextAlign.center,),
         ),
-        padding: EdgeInsets.symmetric(horizontal: 10),
-        alignment: Alignment.center,
-        child: Text(text, style: TextStyle(),),
       );
     }
 
-    Widget _filterType(String title, List<String> list) {
+    Widget _imageChild(int index, FilterItems item){
+      return GestureDetector(
+        onTap: (){
+          setState(() {
+            item.isSelected = !item.isSelected;
+          });
+        },
+        child: Container(
+          width: 100,
+          decoration: BoxDecoration(
+            // color: item.isSelected ? Color(0xffD9F3F2) : Color(0xffEBEBEB),
+            borderRadius: BorderRadius.circular(2),
+            // border: Border.all(color: Color(0xffEBEBEB)),
+          ),
+          child: Image.asset(item.imageName, color: item.isSelected ? Colors.cyan : Colors.black,),
+        ),
+      );
+    }
+
+    Widget _filterType(String title, List<FilterItems> list, {double height = 50}) {
       return Container(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -66,12 +102,16 @@ class _MainFilterViewState extends State<MainFilterView> {
               height: 20,
             ),
             Container(
-              height: 50,
+              height: height,
               child: ListView.separated(
                   itemCount: list.length,
                   scrollDirection: Axis.horizontal,
                   itemBuilder: (context, index) {
-                    return _listChild(index, list[index]);
+                    if (height == 50){
+                      return _listChild(index, list[index]);
+                    }else{
+                      return _imageChild(index, list[index]);
+                    }
                   },
                 separatorBuilder: (BuildContext context, int index) {
                     return SizedBox(width: 10,);
@@ -85,18 +125,18 @@ class _MainFilterViewState extends State<MainFilterView> {
     Widget _allColumnWidget(){
       return Container(
         padding: EdgeInsets.all(20),
-        height: Get.height * 0.5,
+        height: Get.height * 0.45,
         child: Column(
           // mainAxisAlignment: MainAxisAlignment.start,
           // crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             _cancelApplyView(),
             SizedBox(height: 20,),
-            _filterType('Tags', tags),
+            _filterType('Jewellery', widget.jewellery),
             SizedBox(height: 40,),
-            _filterType('Categories', cat),
+            _filterType('Center Shape', widget.centerShape, height: 100),
             SizedBox(height: 40,),
-            _filterType('Attributes', cat),
+            _filterType('Metal', widget.metal),
           ],
         ),
       );
--- lib/search/search_view.dart
+++ lib/search/search_view.dart
@@ -1,13 +1,24 @@
 import 'package:ecom/search/main_filter_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 import '../Head_footer/appbar.dart';
+import '../Head_footer/drawer.dart';
+import '../constant.dart';
 import '../horizontal_list.dart';
+
+class FilterItems{
+  String name;
+  int id;
+  bool isSelected;
+  String imageName;
+
+  FilterItems(this.name, this.id, {this.isSelected = false, this.imageName = ''});
+}
 
 class SearchView extends StatefulWidget {
   const SearchView({Key? key}) : super(key: key);
-
   @override
   State<SearchView> createState() => _SearchViewState();
 }
@@ -15,7 +26,39 @@ class SearchView extends StatefulWidget {
 class _SearchViewState extends State<SearchView> {
 
   TextEditingController txtSearch = TextEditingController();
-  List<String> filters = ['+ Filter'];
+  List<FilterItems> filters = [FilterItems('+ Filter', 0)];
+
+
+  List<FilterItems> jewellery = [
+    FilterItems('Engagement Rings', 1201),
+    FilterItems('Wedding Bands', 1202),
+    FilterItems('Fashion Rings', 1203),
+    FilterItems('Bracelets', 1204),
+    FilterItems('Necklace', 1205),
+    FilterItems('Pendants', 1206),
+    FilterItems('Earrings', 1207),
+  ];
+
+
+  List<FilterItems> centerShape = [
+    FilterItems('Round', 0, imageName: AppIcons.diamond_round),
+    FilterItems('Princess', 0, imageName: AppIcons.diamond_princess),
+    FilterItems('Marquise', 0, imageName: AppIcons.diamond_marquise),
+    FilterItems('Cushion', 0, imageName: AppIcons.diamond_cushion),
+    FilterItems('Oval', 0, imageName: AppIcons.diamond_oval),
+    FilterItems('Pear', 0, imageName: AppIcons.diamond_pear),
+    FilterItems('Emerald', 0, imageName: AppIcons.diamond_emerald),
+    FilterItems('Asscher', 0, imageName: AppIcons.diamond_asscher),
+    FilterItems('Radiant', 0, imageName: AppIcons.diamond_radiant),
+    FilterItems('Heart', 0, imageName: AppIcons.diamond_heart),
+  ];
+
+  List<FilterItems> metal = [
+    FilterItems('White Gold', 0),
+    FilterItems('Yellow Gold', 0),
+    FilterItems('Rose Gold', 0),
+    FilterItems('Platinum', 0),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -81,7 +124,7 @@ class _SearchViewState extends State<SearchView> {
     Widget _listChild(int index){
       if (index == 0){
         return GestureDetector(
-          onTap: (){
+          onTap: () {
             showModalBottomSheet(
                 context: context,
                 shape: RoundedRectangleBorder(
@@ -90,37 +133,60 @@ class _SearchViewState extends State<SearchView> {
                 isScrollControlled: true,
                 enableDrag: false,
                 builder: (BuildContext context) {
-                  return MainFilterView(filters: filters,);
-                });
+                  return MainFilterView(
+                    jewellery: jewellery,
+                    centerShape: centerShape,
+                    metal: metal,
+                  );
+                }).then((value) {
+                  if (value){
+                    print('apply filter');
+                    var selectedJwe = jewellery.filter((element) {return element.isSelected == true;});
+                    var selectedShape = centerShape.filter((element) {return element.isSelected == true;});
+                    var selectedMetal = metal.filter((element) {return element.isSelected == true;});
+
+                    setState(() {
+                      filters = [FilterItems('+ Filter', 0)];
+                      selectedJwe.forEach((element) {filters.add(element);});
+                      selectedShape.forEach((element) {filters.add(element);});
+                      selectedMetal.forEach((element) {filters.add(element);});
+                    });
+                  }else{
+                    setState(() {
+                      filters = [FilterItems('+ Filter', 0)];
+                    });
+                  }
+            });
           },
           child: Container(
             alignment: Alignment.center,
             padding: EdgeInsets.only(right: 15),
-            child: Text(filters[index], style: TextStyle(fontSize: 15, color: Colors.black),),
+            child: Text(filters[index].name, style: TextStyle(fontSize: 15, color: Colors.black),),
           ),
         );
       }else{
         return Container(
-          // color: Colors.black,
+          alignment: Alignment.center,
           padding: EdgeInsets.only(right: 15),
-          child: Row(
-            children: [
-              Text(filters[index], style: TextStyle(fontSize: 15, color: Colors.cyan),),
-              SizedBox(width: 4,),
-              GestureDetector(
-                onTap: (){
-                  setState(() {
-                    filters.removeAt(index);
-                  });
-                },
-                child: Icon(
-                  Icons.cancel,
-                  size: 20,
-                  color: Colors.cyan,
-                ),
-              ),
-            ],
-          ),
+          child: Text(filters[index].name, style: TextStyle(fontSize: 15, color: Colors.cyan),),
+          // child: Row(
+          //   children: [
+          //     Text(filters[index].name, style: TextStyle(fontSize: 15, color: Colors.cyan),),
+          //     SizedBox(width: 4,),
+          //     GestureDetector(
+          //       onTap: (){
+          //         setState(() {
+          //           filters.removeAt(index);
+          //         });
+          //       },
+          //       child: Icon(
+          //         Icons.cancel,
+          //         size: 20,
+          //         color: Colors.cyan,
+          //       ),
+          //     ),
+          //   ],
+          // ),
         );
       }
     }
@@ -164,6 +230,9 @@ class _SearchViewState extends State<SearchView> {
 
 
     return Scaffold(
+      drawer: Drawer(
+        child: drawer(),
+      ),
       appBar: customappbar(),
       body: GestureDetector(
           onTap: () {
